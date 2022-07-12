22aa124121108ae2da9b2652aa2147919b02d647
lucasramon <lucas.ramon.jc@gmail.com>
2022-07-12 13:44:00 -0300

    habilitando CORS

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,16 @@
 import { ApolloServer } from "apollo-server";
 import typeDefs from "./prisma/schema"
 import resolvers from "./resolvers"
+const cors = require('cors')
 
 const port = process.env.PORT || 9090;
 
-const server = new ApolloServer({ resolvers, typeDefs });
+const server = new ApolloServer({
+    cors: {
+        origin: '*',
+        credentials: true
+    }, resolvers, typeDefs
+});
 
 
 server.listen({ port }, () => console.log(`Server runs at: http://localhost:${port}`));
